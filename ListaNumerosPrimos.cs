222d4da9403f717e3bf3ff91ba14e5edbafd563a
Evelyn Correa <evelynnave2015@gmail.com>
2016-07-01 10:45:34 -0300

    Primos
    Trabalho do Lolo - 01/07/2016

---
+++ ListaNumerosPrimos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerosprimos_2003_Evelyn_Flavia_JoaoPedro
+{
+    class ListaNumerosPrimos
+    {
+       public List<int> nprimos = new List<int>();
+
+       public void Lista(int n)
+       {
+           for (int i = 2; i <= n; i++)
+           {
+               nprimos.Add(i);
+               //Console.WriteLine(i);
+           }
+       }
+
+        public void Primos()
+       {
+           for (int i = 0; i < nprimos.Count; i++)
+           {
+               int ElementList = nprimos.ElementAt(i);
+
+               for (int j = i + 1; j < nprimos.Count; j++)
+               {
+                   int DivisibleElements = nprimos.ElementAt(j);
+
+                   if (DivisibleElements % ElementList == 0)
+                   {
+                       nprimos.RemoveAt(j);
+                   }
+               }
+           }
+       }
+        public string PrintPrimos()
+        {
+           string primeNumbersPrinted = "Prime Numbers: ";            
+            foreach (var item in nprimos)
+            {
+                primeNumbersPrinted += item + " ; ";
+		 
+	        }
+            return primeNumbersPrinted;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerosprimos_2003_Evelyn_Flavia_JoaoPedro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaNumerosPrimos numbers = new ListaNumerosPrimos();
+            int Limit = int.Parse(Console.ReadLine());
+            Console.WriteLine("Limit's User " + Limit);
+            numbers.Lista(Limit);
+            numbers.Primos();
+            Console.WriteLine(numbers.PrintPrimos());
+            Console.ReadKey();
+        }
+    }
+}
